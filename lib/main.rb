95782ac3fe232f2a407d8b076365f6bb11af3402
Alessandro Di Maria <adm@m42.ch>
2013-02-11 00:07:10 +0100

    add Guard actor to obtain lock

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,37 @@
 require 'celluloid'
 
+class Guard
+  include Celluloid
+
+  trap_exit :pool_died
+
+  def initialize
+    @locks = {}
+  end
+
+  def to_key *args
+    args.first
+  end
+
+  def obtain requester, method, *args
+    logos = @locks[to_key(args)]
+    unless logos
+      puts "requester #{requester.object_id} obtained key #{to_key(args)}"
+      @locks[to_key(args)] = requester.future.send(method)
+    else
+      puts "requester #{requester.object_id} waits for key #{to_key(args)}"
+      logos.value # wait for requester to finish
+      @locks[to_key(args)] = nil
+      obtain requester, method, *args # retry
+    end
+  end
+
+  def pool_died actor, reason
+    puts "queue pool died #{actor} #{reason} #{current_actor.mailbox}"
+  end
+
+end
+
 class SimObject
 
   attr_accessor :state, :name
@@ -7,6 +39,10 @@ class SimObject
   def initialize name
     self.name  = name
     self.state = 0
+  end
+
+  def get_key
+    :a_key
   end
 
   def sim
@@ -21,11 +57,16 @@ class SimObject
 
 end
 
-class Worker
+class QueueWorker
   include Celluloid
 
-  def sim object
-    object.sim
+  def get_lock object
+    @object = object
+    Celluloid::Actor[:guard].obtain(current_actor, :sim, @object.get_key)
+  end
+
+  def sim
+    @object.sim
   end
 
   def finalize
@@ -42,7 +83,7 @@ class MyQueue
   trap_exit :pool_died
 
   def initialize
-    @pool = Worker.pool_link
+    @pool = QueueWorker.pool_link
     @objects = []
     @enumerator = @objects.each
   end
@@ -70,7 +111,7 @@ class MyQueue
   end
 
   def pool_died actor, reason
-    puts "pool died #{actor} #{reason} #{current_actor.mailbox}"
+    puts "queue pool died #{actor} #{reason} #{current_actor.mailbox}"
   end
 
   def next
@@ -79,7 +120,7 @@ class MyQueue
       if @pool.idle_size > 0
         begin
           object = @enumerator.next
-          @pool.sim!(object)
+          @pool.get_lock!(object)
           self.next!
         rescue StopIteration
           @enumerator.rewind
@@ -99,7 +140,7 @@ class MyQueue
 
 end
 
-
+Celluloid::Actor[:guard] = Guard.new
 queue = MyQueue.new
 
 queue.start
@@ -109,7 +150,7 @@ queue.start
   sleep 1
 end
 
-sleep 8
+sleep 6
 
 queue.stop
 
