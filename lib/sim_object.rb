32cc258a9f497720041014226a0e5949ff3d64c1
Alessandro Di Maria <adm@m42.ch>
2013-02-11 22:32:59 +0100

    extract classes

---
+++ lib/sim_object.rb
@@ -0,0 +1,24 @@
+class SimObject
+
+  attr_accessor :state, :name
+
+  def initialize name
+    self.name  = name
+    self.state = 0
+  end
+
+  def get_key
+    :a_key
+  end
+
+  def sim
+    5.times do |i|
+      self.state += 1
+      puts "sim #{name} #{state}"
+      sleep 1
+    end
+    puts 'done!'
+    #raise Exception, "CRASH"
+  end
+
+end
---
+++ lib/sim_worker.rb
@@ -0,0 +1,17 @@
+class SimWorker
+  include Celluloid
+
+  def get_lock object
+    @object = object
+    Celluloid::Actor[:guard].obtain(current_actor, :sim, @object.get_key)
+  end
+
+  def sim
+    @object.sim
+  end
+
+  def finalize
+    puts "worker #{object_id} stopped"
+  end
+
+end
