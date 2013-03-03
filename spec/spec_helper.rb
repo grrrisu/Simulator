5972abd2823b285446411cf856b4694171dc2e0d
Alessandro Di Maria <adm@m42.ch>
2013-03-03 14:26:52 +0100

    config skip specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,5 +20,6 @@ RSpec.configure do |config|
   config.fail_fast = true
 
   config.filter_run :focus => true
+  config.filter_run_excluding :skip => true
   config.run_all_when_everything_filtered = true
 end
