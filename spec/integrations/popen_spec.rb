8f796ae00e7c385367ef6143d00e2b72a3bbc64c
Alessandro Di Maria <adm@m42.ch>
2013-03-08 20:15:08 +0100

    begin popen integration test

--- spec/integrations/popen_spec.rb
+++ spec/integrations/popen_spec.rb
@@ -1,9 +1,28 @@
 require 'spec_helper'
 
+class PopenTestLevel < Sim::Level
+
+  def process_message message
+    if message == 'see all the stars'
+      message.reverse
+    else
+      super
+    end
+  end
+
+end
+
 describe "popen" do
 
-  it "should create a subprocess", skip: true do
-    level = Sim::Popen::SubProcess.start
+  it "should create a subprocess", focus: true do
+    connection  = Sim::Popen::ParentConnection.new
+    sim_library = File.expand_path('../../../sim.rb', __FILE__)
+    level_class = 'PopenTestLevel'
+    config_file = File.expand_path('../../../level.yml', __FILE__)
+    connection.start(sim_library, level_class, config_file)
+    answer = connection.send_message 'see all the stars'
+    answer.should == 'see all the stars'.reverse
+    connection.close
   end
 
 end
