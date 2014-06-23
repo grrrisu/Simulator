8dd7fe693be6daecfb096df4c4ce7d386abb8fa7
Alessandro Di Maria <adm@m42.ch>
2014-06-24 01:05:23 +0200

    player connection reader and writer

--- lib/sim/unix_socket/player_connection.rb
+++ lib/sim/unix_socket/player_connection.rb
@@ -8,9 +8,38 @@ module Sim
         self.input, self.output = socket, socket
         @player, player.connection = player, self
         @player.register(receive_data)
+        Reader.new(self).async.listen
+        Writer.new(self).async.send_time
       end
 
-      def listen
+      class Reader
+        include Celluloid
+        include Celluloid::Logger
+
+        def initialize connection
+          @connection = connection
+        end
+
+        def listen
+          loop do
+            data = @connection.receive_data
+            $stderr.puts "**** player received #{data}"
+          end
+        end
+
+      end
+
+      class Writer
+        include Celluloid
+        include Celluloid::Logger
+
+        def initialize connection
+          @connection = connection
+        end
+
+        def send_time
+          every(2) { @connection.send_data time: "Now it's #{Time.now.iso8601}" }
+        end
       end
 
     end
