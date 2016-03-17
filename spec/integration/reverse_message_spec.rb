713a9a16833fc3eb6f33acdcf0496f618fa8ace1
Alessandro Di Maria <adm@m42.ch>
2016-03-18 00:32:32 +0100

    + reverse integration test

---
+++ spec/integration/reverse_message_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+module Sim
+  describe 'should process a message' do
+
+    let(:socket)     { double('Socket') }
+    let(:connection) { Net::PlayerConnection.new(socket) }
+
+    before(:each) do
+      Celluloid::Actor[:event_queue] = Queue::EventQueue.new
+      Celluloid::Actor[:level] = Level.new
+      Celluloid::Actor[:broadcaster] = Net::Broadcaster.new
+    end
+
+    it "and return the reversed text" do
+      expect(socket).to receive(:print).with('{"scope":"test","action":"reverse","answer":"dlrow olleh"}')
+      connection.receive '{"scope":"test","player_id":123,"action":"reverse","args":"hello world"}'
+      sleep 0.1
+    end
+
+  end
+end
