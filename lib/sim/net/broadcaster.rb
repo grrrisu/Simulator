628d8d4514319f132871916d5a15985c9f7f5245
Alessandro Di Maria <adm@m42.ch>
2016-04-08 21:31:58 +0200

    ! fix missing loop

--- lib/sim/net/broadcaster.rb
+++ lib/sim/net/broadcaster.rb
@@ -6,18 +6,18 @@ module Sim
       finalizer :shutdown
       trap_exit :player_crashed
 
-      def broadcast player_ids, message
+      def broadcast_to_players player_ids, message
         player_ids = Array(player_ids)
-        info "broadcast #{message.inspect} to #{player_ids}"
+        debug "broadcast #{message.inspect} to #{player_ids}"
         Session.find_players(player_ids).each do |session|
           session.async.send_message message
         end
       end
-      alias broadcast_to_players broadcast
+      alias broadcast broadcast_to_players
 
       def broadcast_to_sessions sessions_ids, message
         sessions_ids = Array(sessions_ids)
-        info "broadcast #{message.inspect} to #{sessions_ids}"
+        debug "broadcast #{message.inspect} to #{sessions_ids}"
         sessions_ids.each do |session_id|
           session = Session.find session_id
           session.async.send_message message
@@ -25,9 +25,8 @@ module Sim
       end
 
       def broadcast_to_all message
-        info "*** braodcast to all"
-        info Session.registry_keys
-        Session.registry_keys do |key|
+        debug "broadcast to all #{Session.registry_keys}"
+        Session.registry_keys.each do |key|
           Actor[key].async.send_message message
         end
       end
