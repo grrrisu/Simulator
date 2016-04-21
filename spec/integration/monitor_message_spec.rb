fff5d1009ffc22241fa081e58df2f22137d8e5b7
Alessandro Di Maria <adm@m42.ch>
2016-04-21 23:24:10 +0200

    ! fix spec

--- spec/integration/monitor_message_spec.rb
+++ spec/integration/monitor_message_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 module Sim
-  describe 'should monitor' do
+  describe 'monitor' do
 
     let(:socket)     { double('Socket') }
     let(:connection) { Net::PlayerConnection.new(socket) }
@@ -17,16 +17,16 @@ module Sim
       Celluloid::Actor[:broadcaster] = Net::Broadcaster.new
     end
 
-    it "subscribe" do
+    it "should subscribe" do
       expect(socket).to receive(:puts).with('{"scope":"monitor","action":"history","summary":{},"snapshot":{"session_size":1,"event_size":0,"object_size":0,"time_unit":0.0}}')
       connection.receive '{"scope":"monitor","player_id":123,"action":"subscribe"}'
       sleep 0.1
     end
 
-    it "a crash", focus: true do
+    it "should report a crash" do
       allow(socket).to receive(:puts).once # the first history event after subscribing
       connection.receive '{"scope":"monitor","player_id":123,"action":"subscribe"}'
-      expect(socket).to receive(:puts).with('{"scope":"monitor","action":"error","answer":{"event":"/Users/alessandro/develop/test/Simulator/examples/server/event/crash_event.rb:10:in `fire'",\"error\":\"Oh Snap!\"}}')
+      expect(socket).to receive(:puts).with(/\{"scope":"monitor","action":"error","answer":\{"event":.*,"error":"Oh Snap!"\}\}/)
       connection.receive '{"scope":"example","player_id":123,"action":"crash"}'
       sleep 0.1
     end
