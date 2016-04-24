f9c79c56e115cc4841335d71aa84b4c542bde69c
Alessandro Di Maria <adm@m42.ch>
2016-04-24 11:12:57 +0200

    + finish monitor spec

--- spec/models/monitor_spec.rb
+++ spec/models/monitor_spec.rb
@@ -21,10 +21,13 @@ module Sim
       }.to change { monitor.subscribers.size }.by(1)
     end
 
-    describe 'with a subscriber', focus: true do
+    describe 'with a subscriber' do
+
+      before :each do
+        expect(broadcaster).to receive(:async).and_return(broadcaster).at_least(:once)
+      end
 
       it "should remove a subscriber" do
-        expect(broadcaster).to receive(:async).and_return(broadcaster)
         expect(broadcaster).to receive(:broadcast_to_sessions).once
         monitor.subscribe session.session_id
 
@@ -34,7 +37,6 @@ module Sim
       end
 
       it "should inform subscribers about a crash" do
-        expect(broadcaster).to receive(:async).and_return(broadcaster).at_least(:once)
         expect(broadcaster).to receive(:broadcast_to_sessions)
                                .with(Set.new([session.session_id]), hash_including(scope: :monitor, action: :history)).ordered
         expect(broadcaster).to receive(:broadcast_to_sessions)
@@ -45,13 +47,29 @@ module Sim
         monitor.add_error error
       end
 
-      it "should broadcast_history" do
+      it "should calculate events summary" do
+        monitor.subscribe session.session_id
 
+        3.times { monitor.add_event name: 'one' }
+        5.times { monitor.add_event name: 'two' }
+        2.times { monitor.add_event name: 'three' }
+
+        summary = monitor.summary
+        expect(summary['one']).to eq(3)
+        expect(summary['two']).to eq(5)
+        expect(summary['three']).to eq(2)
       end
 
-      it "should gather infos for a snapshot" do
+    end
 
-      end
+    it "should gather infos for a snapshot" do
+      snapshot = monitor.snapshot
+      expect(snapshot).to be == {
+        session_size: 0,
+        event_size: 0,
+        object_size: 0,
+        time_unit: 0.0
+      }
     end
 
   end
