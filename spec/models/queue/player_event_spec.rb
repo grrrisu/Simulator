92a06140aa7d3ef27dcf955910c1fd25da6b9525
Alessandro Di Maria <adm@m42.ch>
2018-08-06 19:54:37 +0200

    add player_event and sim_event specs

---
+++ spec/models/queue/player_event_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+module Sim
+  describe Queue::PlayerEvent do
+
+    it "should execute function" do
+      event = Queue::PlayerEvent.new(123) do |player_id|
+        "player with id #{player_id}"
+      end
+
+      expect(event.fire).to eq("player with id 123")
+    end
+
+  end
+end
